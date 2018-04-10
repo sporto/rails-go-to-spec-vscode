19018ec641d2ac10bdaffd0292c805db8dc649f5
Maxim Filimonov <tpaktopsp@gmail.com>
2018-04-10 17:13:33 +0800

    Change resolvers to match jest pattern. Using the simplest pattern for now

--- src/resolver.test.ts
+++ src/resolver.test.ts
@@ -1,77 +1,52 @@
 //import * as assert from 'assert';
-import test from 'ava';
+import test from "ava";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 //import * as vscode from 'vscode';
-import * as resolver from './resolver';
+import * as resolver from "./resolver";
 
 var testCases = [
-	[
-		'/spec/something/foo_spec.rb',
-		'/app/something/foo.rb',
-	],
-	[
-		'/spec/views/namespace/users/_something.html.erb_spec.rb',
-		'/app/views/namespace/users/_something.html.erb',
-	],
-	[
-		'/spec/views/namespace/users/something.html.haml_spec.rb',
-		'/app/views/namespace/users/something.html.haml',
-	],
-	[
-		'/spec/lib/something/foo_spec.rb',
-		'/lib/something/foo.rb',
-	],
-]
+  ["/app/something/foo.test.js", "/app/something/foo.js"],
+  ["/src/component/foo.test.js", "/src/component/foo.js"]
+];
 
-test("isSpec", (t) => {
-	var testCases = [
-		[
-			'/spec/foo/something_spec.rb',
-			true,
-		],
-		[
-			'/spec/views/something.html.erb_spec.rb',
-			true,
-		],
-		[
-			'/app/foo/something.rb',
-			false,
-		],
-		[
-			'/spec/views/something.html.erb.rb',
-			false,
-		]
-	]
-	t.plan(testCases.length);
+test("isSpec", t => {
+  var testCases = [
+    ["/src/foo/something.test.js", true],
+    ["/src/foo/something.test.jsx", true],
+    ["/src/foo/something.spec.js", true],
+    ["/src/foo/something.js", false],
+    ["/foo/something.js", false]
+  ];
+  t.plan(testCases.length);
 
-	testCases.forEach(function(testCase) {
-		var file = testCase[0];
-		var expected = testCase[1];
-		var res = resolver.isSpec(file);
-		t.is(res, expected);
-	});
+  testCases.forEach(function(testCase) {
+    var file = testCase[0];
+    var expected = testCase[1];
+    var res = resolver.isSpec(file);
+    t.is(res, expected);
+  });
 });
 
-test("specToCode", (t) => {
-	t.plan(testCases.length);
+test("specToCode", t => {
+  t.plan(testCases.length);
 
-	testCases.forEach(function(testCase) {
-		var file = testCase[0];
-		var expected = testCase[1];
-		var res = resolver.specToCode(file);
-		t.is(res, expected);
-	});
+  testCases.forEach(function(testCase) {
+    var file = testCase[0];
+    var expected = testCase[1];
+    var res = resolver.specToCode(file);
+    t.is(res, expected);
+  });
 });
 
-test("codeToSpec", (t) => {
-	t.plan(testCases.length);
+test("codeToSpec", t => {
+  t.plan(testCases.length);
 
-	testCases.forEach(function(testCase) {
-		var file = testCase[1];
-		var expected = testCase[0];
-		var res = resolver.codeToSpec(file);
-		t.is(res, expected);
-	});
+  testCases.forEach(function(testCase) {
+    var file = testCase[1];
+    var expected = testCase[0];
+    var res = resolver.codeToSpec(file);
+    t.is(res, expected);
+  });
 });
--- src/resolver.ts
+++ src/resolver.ts
@@ -1,55 +1,19 @@
 export function getRelated(file) {
-	if (isSpec(file)) {
-		return specToCode(file);
-	} else {
-		return codeToSpec(file);
-	}
+  if (isSpec(file)) {
+    return specToCode(file);
+  } else {
+    return codeToSpec(file);
+  }
 }
 
 export function isSpec(file) {
-	return file.indexOf('_spec.rb') > -1;
+  return file.indexOf(".spec") > -1 || file.indexOf(".test") > -1;
 }
 
 export function codeToSpec(file) {
-	var viewRegex = /erb$|haml$|slim$/
-	var isViewFile = file.match(viewRegex);
-	
-	if (isViewFile) {
-		return file
-			.replace('/app/', '/spec/')
-			.replace('.haml', '.haml_spec.rb')
-			.replace('.erb', '.erb_spec.rb')
-			.replace('.slim', '.slim_spec.rb');
-	}
-	
-	file = file.replace('.rb', '_spec.rb');
-	
-	var isLibFile = file.indexOf('/lib/') > -1;
-	if (isLibFile) {
-		return file.replace('/lib/', '/spec/lib/');
-	}
-	
-	return file.replace('/app/', '/spec/');
+  return file.replace(".js", ".test.js");
 }
 
 export function specToCode(file: string) {
-
-	var viewRegex = /(.erb|.haml|.slim)_spec.rb$/;
-	
-	var isViewFile = file.match(viewRegex);
-	if (isViewFile) {
-		return file
-			.replace('_spec.rb', '')
-			.replace('/spec', '/app');
-	}
-
-	file = file.replace('_spec.rb', '.rb');
-
-	var isLibFile = file.indexOf('/spec/lib/') > -1;
-	if (isLibFile) {
-		return file.replace('/spec/lib/', '/lib/');
-	}
-
-	return file.replace('/spec/', '/app/');
+  return file.replace(".test.js", ".js");
 }
-
